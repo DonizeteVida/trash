3cecbd7f3ee99223c42890d64b55f872fc1523a0
Donizete Vida <donizetevida1998@outlook.com>
2022-10-03 16:07:38 -0300

    Map is for ask

--- cpp/exercises/two_sum/main.cpp
+++ cpp/exercises/two_sum/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+std::vector<int> twoSum(std::vector<int> &nums, int target)
+{
+    std::map<int, int> mapped;
+    for (int index = 0; index < nums.size(); index++) {
+        mapped[nums[index]] = index;
+    }
+    std::vector<int> result;
+    for (int index = 0; index < nums.size(); index++)
+    {   
+        int diff = target - nums[index];
+        auto find = mapped.find(diff);
+        if (find != mapped.end() && index != find->second) {
+            result.push_back(index);
+            result.push_back(find->second);
+            break;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    std::vector<int> data = {3, 2, 4};
+    auto res = twoSum(data, 6);
+    for (int i : res) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 }
